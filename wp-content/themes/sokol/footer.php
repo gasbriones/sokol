6543c4f93981bc6ce541233be6c8199cc787b94e
Gaston <briones.gaston@gmail.com>
2016-10-03 13:12:30 -0300

    pie de agencia

--- wp-content/themes/sokol/footer.php
+++ wp-content/themes/sokol/footer.php
@@ -1,5 +1,8 @@
 <footer id="footer" class="footer wrapper">
     <?php include "nav.php"?>
+    <div class="copy">
+        2016 ® Estudio Jurídico Claria & Sokol y Asoc -  Todos los derechos reservados | Diseño Web: <a href="http://swapdg.com/">SWAP design</a>
+    </div>
     <?php wp_footer(); ?>
     <script src="<?php echo get_template_directory_uri(); ?>/js/libs/jquery-2.1.3.min.js"></script>
     <script src="<?php echo get_template_directory_uri(); ?>/js/libs/animatescroll.min.js"></script>
